34bcbaee904e3ac194db866d11fcabdf592f0c0e
yonayvfixico <emily.fixico@gctcok.edu>
2023-09-29 13:59:47 -0500

    Initial commit

---
+++ Game.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQYF
+{
+    /// <summary>
+    /// Game class is made to better understand LINQ
+    /// Properties with automatic getters, string char Esrb, & string Genre
+    /// A constructor that passes the Params title, esrb, & genre
+    /// Each Param is assigned to the corresponding property
+    /// </summary>
+    internal class Game
+    {
+        public string Title { get; }
+
+        public char Esrb { get; }
+
+        public string Genre { get; }
+
+        public Game(string titleParam, char esrbParam, string genreParam)
+        {
+            Title = titleParam; 
+            Esrb = esrbParam;
+            Genre = genreParam;
+        }
+
+
+
+
+
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace LINQYF
+{
+    internal class Program
+    {
+        /// <summary>
+        /// An array of Games is called games & initialized with 10 games
+        /// A LINQ Query is made to get all games with less than 9 characters in the title. & assigned to a var called shortGames
+        /// Select prints "Game Title: <game.Title>.ToUpper()."
+        /// Each element in shortGames is looped through and printed
+        /// A LINQ method query is used to ofind the game "Minecraft" from the games array & assign it to var called mineCraft
+        /// All the game properties of mineCraft are printed to console using the FIrstOrDefault method
+        /// A var tRated selects the titles where the esrb rating is 'T'
+        /// "T Rated Games" is printed to console
+        /// tRated elements are looped through and printed to console
+        /// "E Rated Action Games" is printed to console
+        /// eRatedAction is looped through and each element is printed to console
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Game[] games = new Game[]
+            {
+                new Game("Minecraft", 'E', "Action-Adventure"),
+                new Game("CS:GO", 'M', "FPS"),
+                new Game("Elden Ring", 'M', "Action-Adventure"),
+                new Game("Valorant", 'T', "FPS"),
+                new Game("Halo 3", 'M', "Action FPS"),
+                new Game("ZombCube", 'E', "FPS Surivial"),
+                new Game("Magnet Destroyer", 'E', "Hyper-Casual"),
+                new Game("Paddle Balls", 'E', "Arcade Casual "),
+                new Game("Rocket League", 'E', "Action Racing"),
+                new Game("Fifa 22", 'E', "Sports"),
+            };
+
+            var shortGames = from game in games
+                             where game.Title.Length < 9
+                             select $"Game Title: {game.Title.ToUpper()}";
+            foreach ( var game in games )
+            {
+                Console.WriteLine(game);    
+            }
+
+            var mineCraft = games.Where(game => game.Title == "Minecraft")
+                           .Select(game => $"Title: {game.Title}. ESRB: {game.Esrb}, Genre: {game.Genre}");
+            Console.WriteLine(mineCraft.FirstOrDefault());
+
+            var tRated = from game in games
+                         where game.Esrb == 'T'
+                         select game.Title;
+            Console.WriteLine("T Rated Games:");
+            foreach(var game in tRated)
+            {
+                Console.WriteLine(game);
+            }
+
+            var eRatedAction = from game in games
+                               where game.Esrb == 'E' && game.Genre.Contains("Action")
+                               select game.Title;
+            Console.WriteLine("E Rated Games:");
+            foreach (var game in eRatedAction)
+            {
+                Console.WriteLine(game);
+            }
+
+        }
+    }
+}
